ba4b6a4f3931eafad4effcebe61b917d4b82a5f8
Safwan Shaheer <devorein00@gmail.com>
2021-05-29 19:24:14 +0600

    Created ListTable footer subcomponent

--- src/components/Play/PlayListTable/PlayListTable.tsx
+++ src/components/Play/PlayListTable/PlayListTable.tsx
@@ -6,6 +6,7 @@ import "./PlayListTable.scss";
 export function PlayListTable() {
   const { filteredQuizzes } = useContext(PlayContext);
   return <div className="PlayListTable">
-    <ListTable generateTitle={(item) => `${item.subject} - ${item.title}`} headers={["Beginner", "Intermediate", "Advanced"]} itemKey={"questions"} itemKeyKey={"difficulty"} items={filteredQuizzes} />
+    <ListTable className="PlayListTable--difficulty" generateTitle={(item) => `${item.subject} - ${item.title}`} headers={["Beginner", "Intermediate", "Advanced"]} itemKey={"questions"} itemKeyKey={"difficulty"} items={filteredQuizzes} />
+    <ListTable className="PlayListTable--type" generateTitle={(item) => `${item.subject} - ${item.title}`} headers={["MCQ", "MS", "FIB", "Snippet"]} itemKey={"questions"} itemKeyKey={"type"} items={filteredQuizzes} />
   </div>
 }
--- src/shared/ListTable/index.tsx
+++ src/shared/ListTable/index.tsx
@@ -7,6 +7,7 @@ interface Props<T> {
   items: T[]
   itemKey: keyof T
   generateTitle: (item: T) => string
+  className?: string
 }
 
 export default function ListTable<T extends Record<string, any>>(props: Props<T>) {
@@ -20,6 +21,7 @@ export default function ListTable<T extends Record<string, any>>(props: Props<T>
         title: props.generateTitle(item),
         index: index + 1
       };
+
       props.headers.forEach(header => {
         itemMap[header] = 0
       })
@@ -32,16 +34,36 @@ export default function ListTable<T extends Record<string, any>>(props: Props<T>
     // eslint-disable-next-line
   }, [props.items])
 
-  return <div className="ListTable" style={{ backgroundColor: theme.color.base }}>
-    <div className="ListTable-headers" style={{ backgroundColor: theme.color.dark, color: theme.palette.text.secondary }}>
-      <div className="ListTable-headers-row">
+  const aggregateItemsMap: Record<string, number> = {
+    total: itemsMap.length
+  };
+
+  props.headers.forEach(header => {
+    aggregateItemsMap[header] = 0
+  })
+
+  itemsMap.forEach(itemMap => {
+    Object.entries(itemMap).forEach(([key, value]) => {
+      aggregateItemsMap[key] += value;
+    })
+  });
+
+  return <div className={`ListTable${props.className ? ' ' + props.className : ''}`} style={{ backgroundColor: theme.color.base, color: theme.palette.text.secondary }}>
+    <div className="ListTable-headers">
+      <div className="ListTable-headers-row" style={{ backgroundColor: theme.color.dark }}>
         {["Sl", "Quiz", ...props.headers].map(header => <span className={`ListTable-headers-row-item ListTable-headers-row-item--${header}`} key={header}>{header}</span>)}
       </div>
     </div>
-    <div className="ListTable-body" style={{ color: theme.palette.text.secondary }}>
+    <div className="ListTable-body">
       {itemsMap.map((itemMap) => <div key={itemMap._id} className="ListTable-body-row" style={{ backgroundColor: theme.color.light }}>
         {["index", "title", ...props.headers].map(header => <span className={`ListTable-body-row-item ListTable-body-row-item--${header}`} key={header}>{itemMap[header]}</span>)}
       </div>)}
+    </div>
+    <div className="ListTable-footer">
+      <div className="ListTable-headers-row" style={{ backgroundColor: theme.color.dark }}>
+        <span className={`ListTable-headers-row-item ListTable-headers-row-item--blank`}></span>
+        {["total", ...props.headers].map(header => <span className={`ListTable-headers-row-item ListTable-headers-row-item--${header}`} key={header}>{aggregateItemsMap[header]}</span>)}
+      </div>
     </div>
   </div>
 }
