ecabef5c894d0f7eaa0f510b8c9299ffb59c7726
Safwan Shaheer <devorein00@gmail.com>
2021-05-31 16:51:14 +0600

    Refactored theme

--- src/utils/generateTheme.ts
+++ src/utils/generateTheme.ts
@@ -9,183 +9,105 @@ export function generateTheme(theme: 'dark' | 'light') {
   const opposite_dark = theme === 'dark' ? grey[300] : grey[900];
   const opposite_base = theme === 'dark' ? grey[200] : darken(grey[800], 0.25);
   const opposite_light = theme === 'dark' ? lighten(grey[200], 0.5) : grey[800];
+  const textPrimary = theme === 'dark' ? grey[100] : grey[900];
+  const textSecondary = theme === 'dark' ? grey[200] : grey[800];
 
-  if (theme === 'dark')
-    return createMuiTheme({
-      palette: {
-        type: 'dark',
-        text: {
-          primary: grey[100],
-          secondary: grey[200]
-        },
-        background: {
-          default: base
+  const themeOptions: ExtendedThemeOptions = {
+    palette: {
+      type: theme,
+      text: {
+        primary: textPrimary,
+        secondary: textSecondary
+      },
+      background: {
+        default: base
+      }
+    },
+    typography: {
+      fontFamily: `"Fira Sans"`,
+      fontSize: 14
+    },
+    color: {
+      light,
+      dark,
+      base,
+      opposite_dark,
+      opposite_base,
+      opposite_light
+    },
+    overrides: {
+      MuiButton: {
+        contained: {
+          margin: 2.5
         }
       },
-      typography: {
-        fontFamily: `"Fira Sans"`,
-        fontSize: 14
+      MuiInput: {
+        root: {
+          padding: 2.5
+        }
       },
-      color: {
-        light,
-        dark,
-        base,
-        opposite_dark,
-        opposite_base,
-        opposite_light
+      MuiCheckbox: {
+        root: {
+          padding: 0,
+          marginRight: 5
+        }
       },
-      overrides: {
-        MuiButton: {
-          contained: {
-            margin: 2.5
-          }
-        },
-        MuiInput: {
-          root: {
-            padding: 2.5
-          }
-        },
-        MuiCheckbox: {
-          root: {
-            padding: 0,
-            marginRight: 5
-          }
-        },
-        MuiIconButton: {
-          root: {
-            width: '18px',
-            height: '18px'
-          }
-        },
-        MuiButtonBase: {
-          root: {
-            padding: 0
-          }
-        },
-        MuiFormControlLabel: {
-          root: {
-            marginLeft: 2.5,
-            marginRight: 2.5,
-            margin: 2.5,
-            padding: 5,
-            backgroundColor: light
-          }
-        },
-        MuiFormLabel: {
-          root: {
-            margin: 2.5,
-            padding: 5
-          }
-        },
-        MuiFormControl: {
-          root: {
-            backgroundColor: light,
-            margin: 2.5,
-            padding: 2.5
-          }
-        },
-        MuiTypography: {
-          root: {
-            color: grey[100]
-          }
-        },
-        MuiInputLabel: {
-          root: {
-            fontWeight: 'bolder',
-            fontSize: '1em',
-            backgroundColor: dark,
-            padding: 5,
-            margin: 2.5
-          }
-        },
-        MuiFormGroup: {
-          root: {
-            backgroundColor: base,
-            margin: 2.5,
-            padding: 2.5
-          }
+      MuiIconButton: {
+        root: {
+          width: '18px',
+          height: '18px'
+        }
+      },
+      MuiButtonBase: {
+        root: {
+          padding: 0
+        }
+      },
+      MuiFormControlLabel: {
+        root: {
+          marginLeft: 2.5,
+          marginRight: 2.5,
+          margin: 2.5,
+          padding: 5,
+          backgroundColor: light
+        }
+      },
+      MuiFormLabel: {
+        root: {
+          margin: 2.5,
+          padding: 5
+        }
+      },
+      MuiFormControl: {
+        root: {
+          backgroundColor: light,
+          margin: 2.5,
+          padding: 2.5
+        }
+      },
+      MuiTypography: {
+        root: {
+          color: textPrimary
+        }
+      },
+      MuiInputLabel: {
+        root: {
+          fontWeight: 'bolder',
+          fontSize: '1em',
+          backgroundColor: dark,
+          padding: 5,
+          margin: 2.5
+        }
+      },
+      MuiFormGroup: {
+        root: {
+          backgroundColor: base,
+          margin: 2.5,
+          padding: 2.5
         }
       }
-    } as ExtendedThemeOptions);
-  else
-    return createMuiTheme({
-      palette: {
-        type: 'light',
-        text: {
-          primary: grey[900],
-          secondary: grey[800]
-        },
-        background: {
-          paper: light,
-          default: base
-        }
-      },
-      color: {
-        light,
-        dark,
-        base,
-        opposite_dark,
-        opposite_base,
-        opposite_light
-      },
-      typography: {
-        fontFamily: `"Fira Sans"`,
-        fontSize: 14
-      },
-      overrides: {
-        MuiButton: {
-          contained: {
-            width: `calc(100% - 10px)`,
-            margin: 5
-          }
-        },
-        MuiCheckbox: {
-          root: {
-            padding: 0,
-            marginRight: 5
-          }
-        },
-        MuiIconButton: {
-          root: {
-            width: '18px',
-            height: '18px'
-          }
-        },
-        MuiButtonBase: {
-          root: {
-            padding: 0
-          }
-        },
-        MuiFormControlLabel: {
-          root: {
-            height: 18,
-            marginLeft: 2.5,
-            marginRight: 2.5,
-            margin: 2.5,
-            padding: 5,
-            backgroundColor: light
-          }
-        },
-        MuiTypography: {
-          root: {
-            color: opposite_dark
-          }
-        },
-        MuiInputLabel: {
-          root: {
-            fontWeight: 'bolder',
-            fontSize: '0.85em',
-            backgroundColor: dark,
-            padding: 5
-          }
-        },
-        MuiFormGroup: {
-          root: {
-            backgroundColor: light,
-            margin: 5,
-            padding: '0px 0px 0px 5px'
-          }
-        }
-      }
-    } as ExtendedThemeOptions);
+    }
+  };
+
+  return createMuiTheme(themeOptions);
 }
