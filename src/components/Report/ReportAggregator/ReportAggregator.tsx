d3d2410057b3d59b965981ba07b0622ee50fc5db
Safwan Shaheer <devorein00@gmail.com>
2021-06-03 12:20:47 +0600

    Saved export aggregator to ls

--- src/components/Report/ReportAggregator/ReportAggregator.tsx
+++ src/components/Report/ReportAggregator/ReportAggregator.tsx
@@ -11,32 +11,53 @@ interface Props {
 
 export function ReportAggregator(props: Props) {
   const { theme } = useThemeSettings();
-  const [reportAggregator, setReportAggregator] = useState<IReportAggregator>({
+  const REPORT_AGGREGATOR: IReportAggregator = JSON.parse(localStorage.getItem('REPORT_AGGREGATOR') ?? JSON.stringify({
     time_allocated: 'AVG',
     time_taken: 'AVG',
     weight: 'AVG',
     score: 'AVG',
     verdict: 'TRUE',
     hints_used: 'AVG'
-  });
+  }));
+  const [reportAggregator, setReportAggregator] = useState<IReportAggregator>(REPORT_AGGREGATOR);
 
   const totalWeight = props.filteredResults.reduce((acc, filteredResult) => acc + filteredResult.question.weight, 0);
 
   return <div className="Report-Aggregator" style={{ color: theme.palette.text.primary }}>
     {([['Time Allocated', 'time_allocated'], ['Weight', 'weight']] as const).map(key => <div className="Report-Aggregator-item" key={key[0]}>
-      <Select menuItemLabel={(item) => item} label={key[0]} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={key[1]} state={reportAggregator} />
+      <Select onChange={(e) => {
+        localStorage.setItem('REPORT_AGGREGATOR', JSON.stringify({
+          ...reportAggregator,
+          [key[1]]: e.target.value
+        }))
+      }} menuItemLabel={(item) => item} label={key[0]} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={key[1]} state={reportAggregator} />
       <div className="Report-Aggregator-item-value" style={{ backgroundColor: theme.color.light }}>{computeNumberDataAggregation(props.filteredResults.map(filteredResult => filteredResult.question[key[1]]), { aggregation: reportAggregator[key[1]] })}</div>
     </div>)}
     {([['Time Taken', 'time_taken'], ['Hints Used', 'hints_used']] as const).map(key => <div className="Report-Aggregator-item" key={key[0]}>
-      <Select menuItemLabel={(item) => item} label={key[0]} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={key[1]} state={reportAggregator} />
+      <Select onChange={(e) => {
+        localStorage.setItem('REPORT_AGGREGATOR', JSON.stringify({
+          ...reportAggregator,
+          [key[1]]: e.target.value
+        }))
+      }} menuItemLabel={(item) => item} label={key[0]} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={key[1]} state={reportAggregator} />
       <div className="Report-Aggregator-item-value" style={{ backgroundColor: theme.color.light }}>{computeNumberDataAggregation(props.filteredResults.map(filteredResult => filteredResult[key[1]]), { aggregation: reportAggregator[key[1]] })}</div>
     </div>)}
     <div className="Report-Aggregator-item">
-      <Select menuItemLabel={(item) => item} label={'Score'} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={"score"} state={reportAggregator} />
+      <Select onChange={(e) => {
+        localStorage.setItem('REPORT_AGGREGATOR', JSON.stringify({
+          ...reportAggregator,
+          score: e.target.value
+        }))
+      }} menuItemLabel={(item) => item} label={'Score'} items={["MAX", "MIN", "AVG"]} setState={setReportAggregator} stateKey={"score"} state={reportAggregator} />
       <div className="Report-Aggregator-item-value" style={{ backgroundColor: theme.color.light }}>{computeNumberDataAggregation(props.filteredResults.map(filteredResult => filteredResult.score.amount), { aggregation: reportAggregator.score, divider: totalWeight })}</div>
     </div>
     <div className="Report-Aggregator-item">
-      <Select menuItemLabel={(item) => item} label={"Verdict"} items={["TRUE", "FALSE"]} setState={setReportAggregator} stateKey={'verdict'} state={reportAggregator} />
+      <Select onChange={(e) => {
+        localStorage.setItem('REPORT_AGGREGATOR', JSON.stringify({
+          ...reportAggregator,
+          verdict: e.target.value
+        }))
+      }} menuItemLabel={(item) => item} label={"Verdict"} items={["TRUE", "FALSE"]} setState={setReportAggregator} stateKey={'verdict'} state={reportAggregator} />
       <div className="Report-Aggregator-item-value" style={{ backgroundColor: theme.color.light }}>{computeBooleanDataAggregation(props.filteredResults.map(filteredResult => filteredResult.verdict), reportAggregator.verdict)}</div>
     </div>
   </div>
