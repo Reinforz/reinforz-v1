eb80f49ae0689def0d790a7d4ae025a61df782e0
Safwan Shaheer <devorein00@gmail.com>
2021-06-03 10:42:47 +0600

    Excluded quizzes menu item shows quiz title

--- src/components/Report/ReportFilter/ReportFilter.tsx
+++ src/components/Report/ReportFilter/ReportFilter.tsx
@@ -20,7 +20,7 @@ interface Props {
 export default function ReportFilter(props: Props) {
   const { setReportFilter, reportFilter } = props;
   const { theme } = useThemeSettings();
-  const { selectedQuizIds } = useContext(PlayContext);
+  const { selectedQuizIds, selectedQuizzes } = useContext(PlayContext);
 
   return <div className="ReportFilter" style={{ backgroundColor: theme.color.dark }}>
     <InputRange label={"Time taken range"} min={0} max={120} setState={setReportFilter} state={reportFilter} stateKey={"time_taken"} />
@@ -28,7 +28,10 @@ export default function ReportFilter(props: Props) {
     <RadioGroup items={["0", "1", "2", "any"]} label={"Hints Used"} setState={setReportFilter} state={reportFilter} stateKey={"hints_used"} />
     <CheckboxGroup label={'Excluded Difficulty'} items={['Beginner', 'Intermediate', 'Advanced']} setState={setReportFilter} stateKey={'excluded_difficulty'} state={reportFilter} />
     <CheckboxGroup label={'Excluded Type'} items={['FIB', 'MS', 'MCQ', "Snippet"]} setState={setReportFilter} stateKey={'excluded_types'} state={reportFilter} />
-    <Select multiple label={"Excluded Quizzes"} items={selectedQuizIds} menuItemLabel={(item) => item} setState={setReportFilter} state={reportFilter} stateKey={"excluded_quizzes"} />
+    <Select multiple label={"Excluded Quizzes"} items={selectedQuizIds} menuItemLabel={(item) => {
+      const selectedQuiz = selectedQuizzes.find(selectedQuiz => selectedQuiz._id === item)!;
+      return `${selectedQuiz.subject} - ${selectedQuiz.topic}`
+    }} setState={setReportFilter} state={reportFilter} stateKey={"excluded_quizzes"} />
     <Select multiple label={"Excluded Columns"}
       renderValue={(selected) => (selected as string[]).map((report_stat, index) => <div key={report_stat + "excluded_columns" + index}>{transformLabel(report_stat)}</div>)}
       items={["quiz", "subject", "question", "type", "difficulty", "verdict", "score", "time_allocated", "time_taken", "answers", "weight", "user_answers", "hints_used"]}
