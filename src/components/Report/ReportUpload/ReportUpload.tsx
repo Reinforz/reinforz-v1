d68272fc01be17a605f09c2e7d00b6ebcfae87a9
Safwan Shaheer <devorein00@gmail.com>
2021-06-03 18:59:42 +0600

    Fixed styling for ReportUpload component

--- src/components/Report/ReportUpload/ReportUpload.tsx
+++ src/components/Report/ReportUpload/ReportUpload.tsx
@@ -2,8 +2,10 @@ import yaml from 'js-yaml';
 import { OptionsObject, useSnackbar } from "notistack";
 import React, { useContext } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { ReportContext } from '../../../context/ReportContext';
 import { useThemeSettings } from '../../../hooks';
 import { IReport } from '../../../types';
+import "./ReportUpload.scss";
 
 const centerBottomErrorNotistack = {
   variant: 'error',
@@ -19,39 +21,30 @@ export function ReportUpload() {
   const { theme } = useThemeSettings();
 
   const onDrop = (acceptedFiles: any) => {
-    let filePromises: Promise<IReport>[] = [];
-
-    acceptedFiles.forEach((file: File) => {
-      const reader = new FileReader();
-      filePromises.push(new Promise((resolve, reject) => {
-        reader.onabort = () => reject('file reading was aborted');
-        reader.onerror = () => reject('file reading has failed');
-        reader.onload = () => {
-          const dotSplitChunks = file.name.split(".");
-          const ext = dotSplitChunks[dotSplitChunks.length - 1]
-          const { result } = reader;
-          if (result) {
-            try {
-              const reportData: IReport = ext.match(/(yaml|yml)/) ? yaml.safeLoad(result.toString()) : JSON.parse(result.toString());
-              resolve(reportData);
-            } catch (err) {
-              enqueueSnackbar(`${file.name} Error: ${err.message}`, centerBottomErrorNotistack)
-            }
-          }
+    const file: File = acceptedFiles[0];
+    const reader = new FileReader();
+    reader.onabort = () => console.error('file reading was aborted');
+    reader.onerror = () => console.error('file reading has failed');
+    reader.onload = () => {
+      const dotSplitChunks = file.name.split(".");
+      const ext = dotSplitChunks[dotSplitChunks.length - 1]
+      const { result } = reader;
+      if (result) {
+        try {
+          const reportData: IReport = ext.match(/(yaml|yml)/) ? yaml.safeLoad(result.toString()) : JSON.parse(result.toString());
+          setReport(reportData);
+        } catch (err) {
+          enqueueSnackbar(`${file.name} Error: ${err.message}`, centerBottomErrorNotistack)
         }
-      }));
-      reader.readAsText(file);
-    });
-
-    Promise.all(filePromises).then(report => {
-      setReport(report)
-    });
+      }
+    }
+    reader.readAsText(file);
   };
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: [".yml", ".yaml", "application/json"] })
 
   return <div style={{ backgroundColor: theme.color.light, color: theme.palette.text.secondary }} className="Report-Upload" {...getRootProps()}>
     <input {...getInputProps()} />
-    <p>click to upload report files (.json or .yaml)</p>
+    <p>Click to upload report files (.json or .yaml)</p>
   </div>
 }
