38f6031623bc929fc137fbd562d72fb3c6b63e5a
Safwan Shaheer <devorein00@gmail.com>
2021-06-03 16:28:17 +0600

    Added ReportSettings component

--- src/components/Report/ReportSettings/ReportSettings.tsx
+++ src/components/Report/ReportSettings/ReportSettings.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { PlayContext } from "../../../context/PlayContext";
 import { StackList } from "../../../shared";
+import { transformTextBySeparator } from "../../../utils";
 
 export function ReportSettings() {
   const { playSettings } = useContext(PlayContext);
   return <div className="Report-Settings">
-    <StackList header={"Play Options"} items={[
-      ['Flatten Mix', playSettings.options.flatten_mix.toString()], ['Disable Timer', playSettings.options.disable_timer.toString()]]} />
+    <StackList header={"Play Options"} items={Object.entries(playSettings.options).map(([key, value]) => [transformTextBySeparator(key), value.toString()])} />
+    <StackList header={"Play Options"} items={Object.entries(playSettings.filters).map(([key, value]) => [transformTextBySeparator(key), Array.isArray(value) ? value.join(",") : value.toString()])} />
   </div>
 }
