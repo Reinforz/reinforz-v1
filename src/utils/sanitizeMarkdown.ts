d0036456fb444a7f31378169defc29a6071a1ddb
Safwan Shaheer <devorein00@gmail.com>
2021-06-04 12:57:58 +0600

    Removed marked and dompurify deps

--- src/utils/sanitizeMarkdown.ts
+++
@@ -1,6 +0,0 @@
-import DOMPurify from 'dompurify';
-import marked from 'marked';
-
-export function sanitizeMarkdown(text: string) {
-  return DOMPurify.sanitize(marked(text));
-}
--- tests/utils/sanitizeMarkdown.test.ts
+++
@@ -1,5 +0,0 @@
-import { sanitizeMarkdown } from '../../src/utils';
-
-it(`Should sanitize and convert markdown to html`, () => {
-  expect(sanitizeMarkdown('hello').trim()).toStrictEqual('<p>hello</p>');
-});
