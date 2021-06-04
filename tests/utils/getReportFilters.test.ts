af4887c33d57de9ae2c477a19aa2122ed819b336
Safwan Shaheer <devorein00@gmail.com>
2021-06-04 21:43:45 +0600

    Added tests for getReportFilters module

---
+++ tests/utils/getReportFilters.test.ts
@@ -0,0 +1,38 @@
+import { getReportFilters } from '../../src/utils';
+import { mockLocalStorage } from '../mockLocalStorage';
+
+const { getItemMock } = mockLocalStorage();
+
+it(`Should work if report filters exists in ls`, () => {
+  getItemMock.mockReturnValueOnce(
+    JSON.stringify({
+      time_taken: [0, 120],
+      verdict: 'false',
+      hints_used: 'any'
+    })
+  );
+  const reportFilters = getReportFilters();
+  expect(reportFilters).toStrictEqual({
+    time_taken: [0, 120],
+    verdict: 'false',
+    hints_used: 'any',
+    excluded_types: [],
+    excluded_difficulty: [],
+    excluded_quizzes: [],
+    excluded_columns: []
+  });
+});
+
+it(`Should work if report filters doesn't exist in ls`, () => {
+  getItemMock.mockReturnValueOnce(null);
+  const reportFilters = getReportFilters();
+  expect(reportFilters).toStrictEqual({
+    time_taken: [0, 120],
+    verdict: 'mixed',
+    hints_used: 'any',
+    excluded_types: [],
+    excluded_difficulty: [],
+    excluded_quizzes: [],
+    excluded_columns: []
+  });
+});
