bba5057f562686cbe141f23de02c19e5d955e721
Safwan Shaheer <devorein00@gmail.com>
2021-06-07 18:58:25 +0600

    Integrated Modal and Preset with settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,17 +1,21 @@
 import React, { useCallback, useContext } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { AiFillHome } from 'react-icons/ai';
+import { FaSave } from 'react-icons/fa';
 import { HiDocumentReport } from 'react-icons/hi';
 import { IoMdCreate } from 'react-icons/io';
 import { useHistory } from 'react-router-dom';
+import shortid from 'shortid';
+import { ModalContext } from '../../context/ModalContext';
 import { SettingsContext } from '../../context/SettingsContext';
 import { useThemeSettings } from '../../hooks';
-import { IconGroup, Select, Toggles } from '../../shared';
+import { Icon, IconGroup, Preset, Select, Toggles } from '../../shared';
 import { transformTextBySeparator } from '../../utils';
 import "./Settings.scss";
 
 function Settings() {
-  const { settings, setSettings } = useContext(SettingsContext);
+  const { settings, setSettings, settingsPresets } = useContext(SettingsContext);
+  const { setModalState } = useContext(ModalContext);
   const history = useHistory();
   const { theme: THEME } = useThemeSettings();
 
@@ -43,7 +47,24 @@ function Settings() {
 
       <div className="Settings" style={{ backgroundColor: THEME.color.base, color: THEME.palette.text.secondary }}>
         <div className="Settings-header" style={{ backgroundColor: THEME.color.dark }}>
-          <div>Settings</div>
+          <div style={{ flex: 1, textAlign: 'center' }}>Settings</div>
+          <div style={{ width: 25, display: 'flex', alignItems: 'center', justifyContent: 'center', cursor: 'pointer' }} >
+            <Icon popoverText="Save current settings as preset">
+              <FaSave fill={THEME.color.opposite_light} size={15} onClick={() => {
+                setModalState([true, <Preset closeModal={() => setModalState([false, null])} label={'Save Settings'} onSave={(input) => {
+                  const currentActivePresetId = shortid();
+                  localStorage.setItem('reinforz.play.settings', JSON.stringify({
+                    current: currentActivePresetId,
+                    presets: [...settingsPresets.presets, {
+                      name: input,
+                      id: currentActivePresetId,
+                      data: settings
+                    }]
+                  }))
+                }} />])
+              }} />
+            </Icon>
+          </div>
         </div>
         <div className="Settings-content" style={{ backgroundColor: THEME.color.dark }}>
           <Select items={["light", "dark", "polar_night", "snow_storm"]} label={"Theme"} setState={setSettings} state={settings} stateKey={"theme"} menuItemLabel={(item) => transformTextBySeparator(item)} />
