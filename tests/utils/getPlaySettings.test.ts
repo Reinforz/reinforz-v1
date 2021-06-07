46b886ed8869991ae0bdc8f190be385c96c1f5a8
Safwan Shaheer <devorein00@gmail.com>
2021-06-07 22:11:51 +0600

    Fixed broken tests

--- tests/utils/getPlaySettings.test.ts
+++ tests/utils/getPlaySettings.test.ts
@@ -1,55 +1,36 @@
-import { getPlaySettings } from '../../src/utils';
+import { IPlaySettingsPreset } from '../../src/types';
+import {
+  createDefaultPlaySettingsFiltersState,
+  createDefaultPlaySettingsOptionsState,
+  getPlaySettingsPresets
+} from '../../src/utils';
 import { mockLocalStorage } from '../mockLocalStorage';
 
 const { getItemMock } = mockLocalStorage();
 
-it(`Should work if play settings exists in ls`, () => {
-  getItemMock.mockReturnValueOnce(
-    JSON.stringify({
-      options: {
-        shuffle_options: false,
-        shuffle_quizzes: true,
-        shuffle_questions: false
-      },
-      filters: undefined
-    })
-  );
-  const playSettings = getPlaySettings();
-  expect(playSettings).toStrictEqual({
-    options: {
-      shuffle_options: false,
-      shuffle_quizzes: true,
-      shuffle_questions: false,
-      instant_feedback: true,
-      flatten_mix: false,
-      partial_score: true,
-      disable_timer: false
-    },
-    filters: {
-      time_allocated: [5, 120],
-      excluded_difficulty: [],
-      excluded_types: []
+const defaultPlaySettingsPreset: IPlaySettingsPreset = {
+  current: 'default',
+  presets: [
+    {
+      name: 'Default',
+      id: 'default',
+      data: {
+        options: createDefaultPlaySettingsOptionsState(),
+        filters: createDefaultPlaySettingsFiltersState()
+      }
     }
-  });
+  ]
+};
+
+it(`Should work if play settings exists in ls`, () => {
+  getItemMock.mockReturnValueOnce(JSON.stringify(defaultPlaySettingsPreset));
+  const playSettingsPresets = getPlaySettingsPresets();
+
+  expect(playSettingsPresets).toStrictEqual(defaultPlaySettingsPreset);
 });
 
 it(`Should work if play settings doesn't exist in ls`, () => {
   getItemMock.mockReturnValueOnce(null);
-  const playSettings = getPlaySettings();
-  expect(playSettings).toStrictEqual({
-    options: {
-      shuffle_options: true,
-      shuffle_quizzes: false,
-      shuffle_questions: true,
-      instant_feedback: true,
-      flatten_mix: false,
-      partial_score: true,
-      disable_timer: false
-    },
-    filters: {
-      time_allocated: [5, 120],
-      excluded_difficulty: [],
-      excluded_types: []
-    }
-  });
+  const playSettingsPresets = getPlaySettingsPresets();
+  expect(playSettingsPresets).toStrictEqual(defaultPlaySettingsPreset);
 });
