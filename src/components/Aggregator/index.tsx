670937d633e72e7a1b03a77efef9b8f51d8c3a84
Safwan Shaheer <devorein00@gmail.com>
2021-06-10 22:03:37 +0600

    Added Aggregator component

---
+++ src/components/Aggregator/index.tsx
@@ -0,0 +1,43 @@
+import { SetStateAction } from "react";
+import { Select } from "..";
+import { useThemeSettings } from "../../hooks";
+import { TBooleanAggregation, TNumberAggregation } from "../../types";
+import { computeBooleanDataAggregation, computeNumberDataAggregation } from "../../utils";
+
+interface Props<T> {
+  header: string
+  items: ({
+    label: string,
+    stateKey: keyof T,
+    data: number[],
+    type: 'number'
+  } | {
+    label: string,
+    stateKey: keyof T,
+    data: boolean[],
+    type: 'boolean'
+  })[]
+  state: T
+  setState: (state: SetStateAction<T>) => void
+}
+
+export default function Aggregator<T>(props: Props<T>) {
+  const { header, items, state, setState } = props;
+  const { theme } = useThemeSettings();
+  <div className="Aggregator" style={{ color: theme.palette.text.primary, backgroundColor: theme.color.base }}>
+    <div className="Aggregator-header" style={{ backgroundColor: theme.color.dark }}>
+      {header}
+    </div>
+    <div className="Aggregator-content" style={{ backgroundColor: theme.color.dark }}>
+      {items.map(item => <div className="Aggregator-content-item">
+        <Select menuItemLabel={(item) => item} label={item.label} items={item.type === "number" ? ["MAX", "MIN", "AVG"] : ["TRUE", "FALSE"]} setState={(aggregator) => {
+          setState({
+            ...state,
+            [item.stateKey]: aggregator
+          })
+        }} stateKey={item.stateKey} state={state} />
+        <div className="Aggregator-content-item-value" style={{ backgroundColor: theme.color.light }}>{item.type === "number" ? computeNumberDataAggregation(item.data, { aggregation: state[item.stateKey] as unknown as TNumberAggregation }) : computeBooleanDataAggregation(item.data, state[item.stateKey] as unknown as TBooleanAggregation)}</div>
+      </div>)}
+    </div>
+  </div>
+}
