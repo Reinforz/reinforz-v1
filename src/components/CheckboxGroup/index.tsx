29bb43aed6e65c52c8823f61602dc1a6a75f53f5
Safwan Shaheer <devorein00@gmail.com>
2021-06-12 12:37:53 +0600

    Added shortcuts for Checkbox group

--- src/components/CheckboxGroup/index.tsx
+++ src/components/CheckboxGroup/index.tsx
@@ -23,20 +23,43 @@ export default function CheckboxGroup<I extends Record<string, any>>(props: Prop
     <InputLabel>{props.label}</InputLabel>
     <div style={{ background: theme.color.dark, display: 'flex', flexDirection: 'column', padding: 2.5, margin: 2.5 }} className="CheckboxGroup-content">
       {props.items.map((item, index) => <FormControlLabel key={item + index} label={item} control={<Checkbox checked={items.includes(item)} name={item} onChange={(e) => {
-        const finalItems = e.target.checked ? items.concat(item) : items.filter(_item => _item !== item);
-        props.onChange && props.onChange(finalItems)
-        props.setState({ ...props.state, [props.stateKey]: finalItems });
-        if (e.target.checked) {
+        e.persist();
+        const { altKey, shiftKey, ctrlKey } = e.nativeEvent as unknown as {
+          altKey: boolean
+          shiftKey: boolean
+          ctrlKey: boolean
+        }
+
+        const { checked } = e.target
+
+        if (checked) {
           settings.sound && sounds.pop_off.play();
         } else {
           settings.sound && sounds.pop_on.play();
         }
 
-        if (props.lsKey) {
-          localStorage.setItem(props.lsKey, JSON.stringify({
-            ...props.state,
-            [props.stateKey]: finalItems
-          }))
+        if (shiftKey && !ctrlKey && !altKey) {
+          props.setState({ ...props.state, [props.stateKey]: (new Array(index + 1)).fill(null).map((_, index) => props.items[index]) });
+        } else if (shiftKey && !ctrlKey && altKey) {
+          const excludedItems = (new Array(index + 1)).fill(null).map((_, index) => props.items[index])
+          props.setState({ ...props.state, [props.stateKey]: items.filter(item => !excludedItems.includes(item)) });
+        } else if (!shiftKey && !ctrlKey && altKey) {
+          if (checked) props.setState({ ...props.state, [props.stateKey]: props.items[index] });
+          else {
+            props.setState({ ...props.state, [props.stateKey]: props.items.filter(item => item !== props.items[index]) })
+          }
+        }
+        else if (!shiftKey && !ctrlKey && !altKey) {
+          const finalItems = checked ? items.concat(item) : items.filter(_item => _item !== item);
+          props.onChange && props.onChange(finalItems)
+          props.setState({ ...props.state, [props.stateKey]: finalItems });
+
+          if (props.lsKey) {
+            localStorage.setItem(props.lsKey, JSON.stringify({
+              ...props.state,
+              [props.stateKey]: finalItems
+            }))
+          }
         }
       }}
         color="primary" />} />)}
