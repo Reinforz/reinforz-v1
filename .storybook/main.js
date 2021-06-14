b4252687da25a5704ca7b83da039c6aeb4c3b8d7
Safwan Shaheer <devorein00@gmail.com>
2021-06-14 11:58:58 +0600

    Added sotry for Boolean and Number Aggregator state

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
+  stories: ['../src/stories/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- src/stories/Aggregator.stories.tsx
+++ src/stories/Aggregator.stories.tsx
@@ -3,17 +3,18 @@ import { Meta, Story } from '@storybook/react';
 import { useState } from 'react';
 import Aggregator, { AggregatorProps } from '../components/Aggregator';
 import { SettingsContext } from '../context/SettingsContext';
-import { ExtendedTheme, TNumberAggregation } from '../types';
+import { ExtendedTheme, TBooleanAggregation, TNumberAggregation } from '../types';
 import { generateDefaultSettingsPreset, generateTheme } from '../utils';
 
 export default {
-  title: 'Example/Aggregator',
+  title: 'Components/Aggregator',
   component: Aggregator,
 } as Meta;
 
 const Template: Story<AggregatorProps<any>> = (args) => {
-  const [state, setState] = useState<Record<string, TNumberAggregation>>({
-    score: 'AVG'
+  const [state, setState] = useState<Record<string, TNumberAggregation | TBooleanAggregation>>({
+    score: 'AVG',
+    verdict: 'TRUE'
   });
   const defaultSettingsPresets = generateDefaultSettingsPreset();
   const defaultSettingsPreset = defaultSettingsPresets.presets[0].data
@@ -26,13 +27,24 @@ const Template: Story<AggregatorProps<any>> = (args) => {
   </ThemeProvider>
 };
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const NumberAggregator = Template.bind({});
+NumberAggregator.args = {
   header: 'Aggregator',
   items: [{
     label: 'Score',
     stateKey: 'score',
     data: [1, 2, 3, 4, 5, 5, 2, 3, 1, 4],
     type: 'number'
+  }],
+};
+
+export const BooleanAggregator = Template.bind({});
+BooleanAggregator.args = {
+  header: 'Aggregator',
+  items: [{
+    label: 'Verdict',
+    stateKey: 'verdict',
+    data: [true, false, true, false, true, true, false],
+    type: 'boolean'
   }],
 };
