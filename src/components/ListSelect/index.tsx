5af5c7c5f25d204ccdd62e75868594a991fbdf04
Safwan Shaheer <devorein00@gmail.com>
2021-06-16 00:47:05 +0600

    Added ListSelect component story

--- src/components/ListSelect/index.tsx
+++ src/components/ListSelect/index.tsx
@@ -1,31 +1,30 @@
 import { FormGroup, InputLabel, MenuItem, Select as MuiSelect } from "@material-ui/core";
 import { ChangeEvent, useContext } from "react";
 import { SettingsContext } from "../../context/SettingsContext";
-import { useThemeSettings } from "../../hooks";
 import sounds from "../../sounds";
+import { transformTextBySeparator } from "../../utils";
 
-interface Props {
+export interface ListSelectProps {
   item: string
   label?: string
   items: string[]
-  menuItemLabel: (item: string) => string
+  menuItemLabel?: (item: string) => string
   onChange: (item: string) => void
 }
 
-export default function ListSelect(props: Props) {
+export default function ListSelect(props: ListSelectProps) {
   const { settings } = useContext(SettingsContext);
-  const { theme } = useThemeSettings();
   const { items, onChange, item, label, menuItemLabel } = props;
-  return <FormGroup>
+  return <FormGroup className="ListSelect pb-0">
     {label && <InputLabel>{label}</InputLabel>}
-    <div style={{ display: 'flex', flexDirection: 'column', padding: 2.5, margin: 2.5 }} className="Select-content bg-light">
+    <div className="Select-content bg-light flex fd-c p-5 mb-5">
       <MuiSelect value={item}
         onChange={(e: ChangeEvent<{ name?: string | undefined; value: unknown }>) => {
           settings.sound && sounds.click.play();
           onChange(e.target.value as string)
         }}>
         {items.map(item =>
-          <MenuItem key={item} value={item}>{menuItemLabel(item)}</MenuItem>
+          <MenuItem key={item} value={item}>{menuItemLabel ? menuItemLabel(item) : transformTextBySeparator(item)}</MenuItem>
         )}
       </MuiSelect>
     </div>
