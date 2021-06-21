ad077d5f3be282a83179bbecae8c2e4b565a695f
Safwan Shaheer <devorein00@gmail.com>
2021-06-21 16:03:20 +0600

    Fixed Styling for Snippet questions

--- src/pages/Question/QuestionInputs/QuestionInputs.tsx
+++ src/pages/Question/QuestionInputs/QuestionInputs.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { useThemeSettings } from '../../../hooks';
 import sounds from "../../../sounds";
 import { TInputQuestionFull } from '../../../types';
-import "./QuestionInputs.scss";
 
 interface Props {
   setUserAnswers: React.Dispatch<React.SetStateAction<string[]>>
@@ -16,21 +15,20 @@ export default function QuestionInputs(props: Props) {
   const { setUserAnswers, userAnswers, question: { _id, type } } = props;
 
   return (
-    <div className={`QuestionInputs QuestionInputs-${type} bg-base`}>
+    <div className={`QuestionInputs QuestionInputs-${type} bg-base p-5 pb-0 flex fd-c`}>
       {props.question.answers.map((_, i) =>
-        <div key={`${_id}.${i}`} className={`QuestionInputs-item`}>
-          <TextField autoFocus={i === 0} fullWidth inputProps={{
-            placeholder: `Answer ${i + 1}`
-          }} value={userAnswers[i] ?? ''} onChange={e => {
-            if (settings.sound) {
-              const min = Math.ceil(1);
-              const max = Math.floor(3);
-              const generatedRandomInt = Math.floor(Math.random() * (max - min + 1)) + min;
-              sounds[`keyboard_${generatedRandomInt}` as 'keyboard_1'].play()
-            }
-            userAnswers[i] = e.target.value;
-            setUserAnswers([...userAnswers])
-          }} /></div>
+        <TextField key={`${_id}.${i}`} className={`QuestionInputs-item mb-5 w-calc_100p_m_10px`} autoFocus={i === 0} fullWidth inputProps={{
+          placeholder: `Answer ${i + 1}`
+        }} value={userAnswers[i] ?? ''} onChange={e => {
+          if (settings.sound) {
+            const min = Math.ceil(1);
+            const max = Math.floor(3);
+            const generatedRandomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+            sounds[`keyboard_${generatedRandomInt}` as 'keyboard_1'].play()
+          }
+          userAnswers[i] = e.target.value;
+          setUserAnswers([...userAnswers])
+        }} />
       )}
     </div>
   );
