95540412f7a2f6425303d43c8729048466b8c675
Safwan Shaheer <devorein00@gmail.com>
2021-06-23 23:27:41 +0600

    Created ReportTableRow component

--- src/pages/Report/ReportTable/ReportTable.tsx
+++ src/pages/Report/ReportTable/ReportTable.tsx
@@ -1,55 +1,67 @@
 import { Typography } from "@material-ui/core";
 import { green, red } from "@material-ui/core/colors";
-import { useContext, useMemo } from "react";
+import { ReactNode, useContext, useMemo } from "react";
+import { BsFilm } from "react-icons/bs";
 import { MdDelete } from 'react-icons/md';
 import { Hovertips, Markdown, StackList } from "../../../components";
 import { QuestionDisplay } from "../../../components/QuestionDisplay";
+import { ModalContext } from "../../../context/ModalContext";
 import { ReportContext } from "../../../context/ReportContext";
 import { useThemeSettings } from "../../../hooks";
 import sounds from "../../../sounds";
-import { TResultInputQuestion } from "../../../types";
+import { IResult, TResultInputQuestion } from "../../../types";
 import { ReportAnswers } from "../ReportAnswers/ReportAnswers";
 import { ReportOptions } from "../ReportOptions/ReportOptions";
 import "./ReportTable.scss";
 
+
+function ReportTableRow(props: { result: IResult, reportQuestion: ReactNode, index: number }) {
+  const { settings, theme } = useThemeSettings();
+  const { setModalState } = useContext(ModalContext);
+  const { excludedColumns, setReport, report } = useContext(ReportContext);
+  const { result, reportQuestion, index } = props;
+
+  const showHints = result.question.hints.length !== 0 && !excludedColumns['hints'];
+  return <div key={result.question._id} className="Report-Table-item pb-0 p-5 bg-dark">
+    <div className="flex ai-c jc-c bg-dark mb-5">
+      {!excludedColumns['quiz_info'] ? <Typography className="p-10 bg-light fs-16 bold">{`${result.question.quiz.subject} - ${result.question.quiz.topic}`}</Typography> : null}
+      <Typography variant="h6" className="Report-Table-item-index bold flex-1 ta-c">{index + 1}</Typography>
+      <div className="Report-Table-item-modal flex jc-sb c-p ai-c" style={{ width: 40 }}><Hovertips popoverText="View separate"><BsFilm fill={theme.color.opposite_dark} size={15} onClick={() => {
+        setModalState([true, <div>Hello World</div>])
+      }} /></Hovertips>
+        <Hovertips popoverText="Delete"><MdDelete size={20} fill={red[500]} onClick={() => {
+          settings.sound && sounds.remove.play();
+          setReport({
+            ...report,
+            results: report.results.filter(_result => _result._id !== result._id)
+          })
+        }} /></Hovertips></div>
+    </div>
+    {reportQuestion}
+    <div className="Report-Table-item-stats mb-5">
+      {!excludedColumns['question_stats'] ? <StackList header="Question Stats" items={[['Type', result.question.type], ['Difficulty', result.question.difficulty], ['Time Allocated', result.question.time_allocated], ['Weight', result.question.weight]]} classNames={{ container: 'mr-5' }} /> : null}
+      {!excludedColumns['user_stats'] ? <StackList classNames={{ container: 'mr-5' }} header="User Stats" items={[['Time Taken', result.time_taken], ['Hints Used', result.hints_used], ['Verdict', <Typography className="bold" style={{
+        color: result.verdict === false ? red[500] : green[500]
+      }}>{result.verdict === false ? "Incorrect" : "Correct"}</Typography>]]} /> : null}
+      {!excludedColumns['score_breakdown'] ? <StackList header="Score Breakdown" items={[['Amount', result.score.amount], ['Answers', result.score.answers], ['Time', result.score.time], ['Hints', result.score.hints], ['Weighted', result.question.weight * result.score.amount]]} /> : null}
+    </div>
+    <div className="flex">
+      {(result.question.type === "MCQ" || result.question.type === "MS") ? !excludedColumns['options'] ? <ReportOptions question={result.question} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null : !excludedColumns['answers'] ? <ReportAnswers question={result.question as TResultInputQuestion} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null}
+      {showHints ? <div className="Report-Table-item-hints bg-base p-5 mb-5" style={{ width: '25%' }}>
+        {result.question.hints.map(hint => <div className="Report-Table-item-hints-item bg-light p-5 mb-5" key={hint}>
+          <Markdown content={hint} />
+        </div>)}
+      </div> : null}
+    </div>
+  </div>
+}
+
 export function ReportTable() {
-  const { sortedResults, excludedColumns, setReport, report } = useContext(ReportContext);
+  const { sortedResults, excludedColumns } = useContext(ReportContext);
 
   const memoizedReportQuestions = useMemo(() => sortedResults.map(sortedResult => <QuestionDisplay question={sortedResult.question} userAnswers={sortedResult.user_answers} showImage={Boolean(!excludedColumns["image"] && sortedResult.question.image)} showQuestion={!excludedColumns["question"]} />), [sortedResults, excludedColumns]);
 
-  const { settings } = useThemeSettings();
   return <div className="Report-Table p-5 bg-base overflowY-auto">
-    {sortedResults.map((sortedResult, index) => {
-      const showHints = sortedResult.question.hints.length !== 0 && !excludedColumns['hints'];
-      return <div key={sortedResult.question._id} className="Report-Table-item pb-0 p-5 bg-dark">
-        <div className="flex ai-c jc-c bg-dark mb-5">
-          {!excludedColumns['quiz_info'] ? <Typography className="p-10 bg-light fs-16 bold">{`${sortedResult.question.quiz.subject} - ${sortedResult.question.quiz.topic}`}</Typography> : null}
-          <Typography variant="h6" className="Report-Table-item-index bold flex-1 ta-c">{index + 1}</Typography>
-          <div className="Report-Table-item-delete flex jc-c c-p" style={{ width: 20 }}><Hovertips popoverText="Delete"><MdDelete fill={red[500]} onClick={() => {
-            settings.sound && sounds.remove.play();
-            setReport({
-              ...report,
-              results: report.results.filter(result => result._id !== sortedResult._id)
-            })
-          }} /></Hovertips></div>
-        </div>
-        {memoizedReportQuestions[index]}
-        <div className="Report-Table-item-stats mb-5">
-          {!excludedColumns['question_stats'] ? <StackList header="Question Stats" items={[['Type', sortedResult.question.type], ['Difficulty', sortedResult.question.difficulty], ['Time Allocated', sortedResult.question.time_allocated], ['Weight', sortedResult.question.weight]]} classNames={{ container: 'mr-5' }} /> : null}
-          {!excludedColumns['user_stats'] ? <StackList classNames={{ container: 'mr-5' }} header="User Stats" items={[['Time Taken', sortedResult.time_taken], ['Hints Used', sortedResult.hints_used], ['Verdict', <Typography className="bold" style={{
-            color: sortedResult.verdict === false ? red[500] : green[500]
-          }}>{sortedResult.verdict === false ? "Incorrect" : "Correct"}</Typography>]]} /> : null}
-          {!excludedColumns['score_breakdown'] ? <StackList header="Score Breakdown" items={[['Amount', sortedResult.score.amount], ['Answers', sortedResult.score.answers], ['Time', sortedResult.score.time], ['Hints', sortedResult.score.hints], ['Weighted', sortedResult.question.weight * sortedResult.score.amount]]} /> : null}
-        </div>
-        <div className="flex">
-          {(sortedResult.question.type === "MCQ" || sortedResult.question.type === "MS") ? !excludedColumns['options'] ? <ReportOptions question={sortedResult.question} userAnswers={sortedResult.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null : !excludedColumns['answers'] ? <ReportAnswers question={sortedResult.question as TResultInputQuestion} userAnswers={sortedResult.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null}
-          {showHints ? <div className="Report-Table-item-hints bg-base p-5 mb-5" style={{ width: '25%' }}>
-            {sortedResult.question.hints.map(hint => <div className="Report-Table-item-hints-item bg-light p-5 mb-5" key={hint}>
-              <Markdown content={hint} />
-            </div>)}
-          </div> : null}
-        </div>
-      </div>
-    })}
+    {sortedResults.map((sortedResult, index) => <ReportTableRow index={index} result={sortedResult} reportQuestion={memoizedReportQuestions[index]} />)}
   </div>
 }
