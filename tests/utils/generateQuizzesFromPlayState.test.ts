936ff6ade4394b29f17601fe64d038b04592f2de
Safwan Shaheer <devorein00@gmail.com>
2021-06-24 19:47:23 +0600

    Flxed generateQuizzesFromPlayState module

--- tests/utils/generateQuizzesFromPlayState.test.ts
+++ tests/utils/generateQuizzesFromPlayState.test.ts
@@ -1,19 +1,21 @@
+import { IQuizFull } from '../../src/types';
 import { generateQuizzesFromPlayState } from '../../src/utils';
+
+const allQuizzesMap: Map<string, IQuizFull> = new Map([
+  ['quiz.1', { _id: 'quiz.1' } as any]
+]);
 
 it(`Should work`, () => {
   const question_1: any = {
       _id: 'question.1',
-      quiz: {
-        _id: 'quiz.1'
-      }
+      quiz: 'quiz.1'
     },
     question_2: any = {
       _id: 'question.2',
-      quiz: {
-        _id: 'quiz.1'
-      }
+      quiz: 'quiz.1'
     };
   const generatedQuizMap = generateQuizzesFromPlayState(
+    allQuizzesMap,
     {
       questions: [question_1, question_2]
     } as any,
