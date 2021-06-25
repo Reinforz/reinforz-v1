8b8bab4eda449fc4ed52d1a09ca20095d4a75c75
Safwan Shaheer <devorein00@gmail.com>
2021-06-25 15:43:26 +0600

    Added styling for ReportTable

--- src/pages/Report/ReportTable/ReportTable.tsx
+++ src/pages/Report/ReportTable/ReportTable.tsx
@@ -21,7 +21,11 @@ interface ReportTableRowProps {
   reportQuestions: ReactNode[],
   index: number,
   excludedColumns: Record<string, boolean>
-  style?: React.CSSProperties
+  style?: {
+    header?: React.CSSProperties
+    content?: React.CSSProperties
+    container?: React.CSSProperties
+  }
   quiz: string
 };
 
@@ -34,30 +38,32 @@ function ReportTableModal(props: ReportTableRowProps) {
 }
 
 function ReportTableRow(props: ReportTableRowProps) {
-  const { reportQuestions, index, results, header, excludedColumns, style, quiz } = props;
+  const { reportQuestions, index, results, header, excludedColumns, style = {}, quiz } = props;
   const result = results[index];
   const showHints = result.question.hints.length !== 0 && !excludedColumns['hints'];
-  return <div key={result.question._id} className="ReportTable-item pb-0 p-5 bg-dark" style={style}>
-    <div className="flex ai-c jc-c bg-dark mb-5">
+  return <div key={result.question._id} className="ReportTableRow pb-0 p-5 bg-dark h-100p" style={style.container}>
+    <div className="ReportTableRow-header flex ai-c jc-c bg-dark mb-5" style={style.header}>
       {!excludedColumns['quiz_info'] ? <Typography className="p-10 bg-light fs-16 bold">{quiz}</Typography> : null}
-      <Typography variant="h6" className="ReportTable-item-index bold flex-1 ta-c">{index + 1}</Typography>
+      <Typography variant="h6" className="ReportTableRow-index bold flex-1 ta-c">{index + 1}</Typography>
       {header}
     </div>
-    {reportQuestions[index]}
-    <div className="ReportTable-item-stats mb-5">
-      {!excludedColumns['question_stats'] ? <StackList header="Question Stats" items={[['Type', result.question.type], ['Difficulty', result.question.difficulty], ['Time Allocated', result.question.time_allocated], ['Weight', result.question.weight]]} classNames={{ container: 'mr-5' }} /> : null}
-      {!excludedColumns['user_stats'] ? <StackList classNames={{ container: 'mr-5' }} header="User Stats" items={[['Time Taken', result.time_taken], ['Hints Used', result.hints_used], ['Verdict', <Typography className="bold" style={{
-        color: result.verdict === false ? red[500] : green[500]
-      }}>{result.verdict === false ? "Incorrect" : "Correct"}</Typography>]]} /> : null}
-      {!excludedColumns['score_breakdown'] ? <StackList header="Score Breakdown" items={[['Amount', result.score.amount], ['Answers', result.score.answers], ['Time', result.score.time], ['Hints', result.score.hints], ['Weighted', result.question.weight * result.score.amount]]} /> : null}
-    </div>
-    <div className="flex">
-      {(result.question.type === "MCQ" || result.question.type === "MS") ? !excludedColumns['options'] ? <ReportOptions question={result.question} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null : !excludedColumns['answers'] ? <ReportAnswers question={result.question as TResultInputQuestion} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null}
-      {showHints ? <div className="ReportTable-item-hints bg-base p-5 mb-5" style={{ width: '25%' }}>
-        {result.question.hints.map(hint => <div className="ReportTable-item-hints-item bg-light p-5 mb-5" key={hint}>
-          <Markdown content={hint} />
-        </div>)}
-      </div> : null}
+    <div className="ReportTableRow-content" style={style.content}>
+      {reportQuestions[index]}
+      <div className="ReportTableRow-content-stats mb-5">
+        {!excludedColumns['question_stats'] ? <StackList header="Question Stats" items={[['Type', result.question.type], ['Difficulty', result.question.difficulty], ['Time Allocated', result.question.time_allocated], ['Weight', result.question.weight]]} classNames={{ container: 'mr-5' }} /> : null}
+        {!excludedColumns['user_stats'] ? <StackList classNames={{ container: 'mr-5' }} header="User Stats" items={[['Time Taken', result.time_taken], ['Hints Used', result.hints_used], ['Verdict', <Typography className="bold" style={{
+          color: result.verdict === false ? red[500] : green[500]
+        }}>{result.verdict === false ? "Incorrect" : "Correct"}</Typography>]]} /> : null}
+        {!excludedColumns['score_breakdown'] ? <StackList header="Score Breakdown" items={[['Amount', result.score.amount], ['Answers', result.score.answers], ['Time', result.score.time], ['Hints', result.score.hints], ['Weighted', result.question.weight * result.score.amount]]} /> : null}
+      </div>
+      <div className="flex">
+        {(result.question.type === "MCQ" || result.question.type === "MS") ? !excludedColumns['options'] ? <ReportOptions question={result.question} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null : !excludedColumns['answers'] ? <ReportAnswers question={result.question as TResultInputQuestion} userAnswers={result.user_answers} className={`${showHints ? 'mr-5' : ''}`} /> : null}
+        {showHints ? <div className="ReportTableRow-content-hints bg-base p-5 mb-5" style={{ width: '25%' }}>
+          {result.question.hints.map(hint => <div className="ReportTableRow-content-hints bg-light p-5 mb-5" key={hint}>
+            <Markdown content={hint} />
+          </div>)}
+        </div> : null}
+      </div>
     </div>
   </div>
 }
@@ -72,8 +78,13 @@ export function ReportTable() {
       const quiz = report.quizzes[sortedResult.question.quiz];
       return <ReportTableRow quiz={`${quiz.subject} - ${quiz.topic}`} key={sortedResult._id} reportQuestions={memoizedReportQuestions} index={index} results={sortedResults} excludedColumns={excludedColumns} header={<div className="ReportTable-item-icons flex jc-sb c-p ai-c" style={{ width: 40 }}><Hovertips popoverText="View separate"><BsFilm fill={theme.color.opposite_dark} size={15} onClick={() => {
         setModalState([true, <ReportTableModal quiz={`${quiz.subject} - ${quiz.topic}`} style={{
-          height: "100%",
-          overflowY: 'auto'
+          content: {
+            height: "calc(100% - 60px)",
+            overflowY: 'auto'
+          },
+          container: {
+            height: '100%'
+          }
         }} reportQuestions={memoizedReportQuestions} index={index} results={sortedResults} excludedColumns={excludedColumns} header={null} />])
       }} /></Hovertips>
         <Hovertips popoverText="Delete"><MdDelete size={20} fill={red[500]} onClick={() => {
