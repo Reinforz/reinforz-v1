1df56bb94663d6c71797783e7843d9b1903fc8b5
Safwan Shaheer <devorein00@gmail.com>
2022-02-28 17:54:54 +0600

    Fixed imports for tsx components

--- scripts/updateName.js
+++ scripts/updateName.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
+const ts = require("typescript")
 
 const componentsDirPath = path.resolve(__dirname, "../src/components");
 
 const directories = fs.readdirSync(componentsDirPath)
+const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
 
 for (let index = 0; index < directories.length; index++) {
   const directory = directories[index];
@@ -16,6 +18,22 @@ for (let index = 0; index < directories.length; index++) {
       const extension = path.extname(file);
       fs.renameSync(path.join(directoryPath, file), path.join(componentsDirPath, `${directory}${extension}`));
     }
+    const tsxFile = path.join(directoryPath, "index.tsx");
+    const program = ts.createProgram([tsxFile], {});
+    const sourceFile = program.getSourceFile(tsxFile);
+    sourceFile.statements.forEach(statement => {
+      if (statement.kind === 262) {
+        const moduleSpecifierText = statement.moduleSpecifier.text;
+        if (moduleSpecifierText === "./style.scss") {
+          statement.moduleSpecifier.text = `./${directory}.scss`
+        } else if (moduleSpecifierText.startsWith("../")) {
+          statement.moduleSpecifier.text = moduleSpecifierText.split("/").slice(1).join("/");
+        } else if (moduleSpecifierText === "..") {
+          statement.moduleSpecifier.text = `./`
+        }
+      }
+    })
+    fs.writeFileSync(path.join(componentsDirPath, `${directory}.tsx`), printer.printFile(sourceFile), "utf-8")
     fs.rmdirSync(directoryPath)
   }
 }
